36f9581532c2e8de62905eb9025102f9a2b9a524
Andrey Kruglov <kruglov.it@gmail.com>
2025-01-13 16:01:54 +0300

    fix: build to deploy

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -13,7 +13,6 @@ module.exports = {
 		filename: production
 			? 'static/scripts/[name].[contenthash].js'
 			: 'static/scripts/[name].js', // имя нашего бандла
-		publicPath: '/',
 	},
 	//Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
 	module: {

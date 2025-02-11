b231c0fa31a62606091780f7c096ca7da5a58f0a
Andrey Kruglov <kruglov.it@gmail.com>
2025-02-11 13:45:51 +0300

    feat: add testing environment and additional packages

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
 declare module '*.svg' {
 	import React = require('react');
 	export const ReactComponent: React.FunctionComponent<
@@ -33,3 +35,5 @@ declare module '*.module.sass' {
 	const classes: { [key: string]: string };
 	export default classes;
 }
+
+/* eslint-enable  @typescript-eslint/no-explicit-any */
